653374c73f16ba94fe4fc3a4694f07d167e55eb1
WatermelonCat <azaelgarcia6868@gmail.com>
2025-07-15 10:56:56 -0500

    n

--- livefeed.py
+++ livefeed.py
@@ -112,7 +112,6 @@ while True:
                         # only show classification if confidence is above threshold
                         if confidence.item() > CONFIDENCE_THRESHOLD:
                             label = classes[predicted.item()]
-                            bin_label = map_to_bin(label)
                             confidence_pct = confidence.item() * 100
 
                             # CATEGORY-SPECIFIC ACTIONS - use motor controller
